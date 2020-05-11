694ab4af3fac0c79db8263adec7ed8fbc8daabac
julian@mumuki.org <julian@mumuki.org>
2020-05-11 01:19:48 +0000

    Mumuki Export on 2020-05-11 01:19:48 +0000

--- 00050_tipos-05/test.hs
+++ 00050_tipos-05/test.hs
@@ -8,3 +8,5 @@ describe "tipos5" $ do
   it "la expresion f1 (&&) True False debería tipar" $ do
     (f1 (&&) True False) `shouldBe` False
 
+  it "la expresion f1 (>) 2 5 debería tipar" $ do
+    (f1 (>) 2 5) `shouldBe` False
